5189f4051de2bf66b451ab1d73a67ee6c0f1022f
AnniNico <nicolo.mandrile@gmail.com>
2019-05-26 13:43:37 +0200

    Added Youtube API

---
+++ app/src/main/java/com/example/android/spotifanni/SearchActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.spotifanni;
+
+import android.app.Activity;
+
+/**
+ * Created by nicolo.mandrile on 26/05/2019.
+ */
+
+public class SearchActivity extends Activity {
+}
